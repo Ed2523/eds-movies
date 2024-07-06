c33eb89f77ac3e15f23bec9c8d6e2dd7b98c4fd1
Edgar Chavez <citylight2392@gmail.com>
2024-07-06 09:40:47 -0600

    setting react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
